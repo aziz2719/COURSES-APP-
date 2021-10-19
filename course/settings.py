a8cc8ee0d4bbcb2188e38031724a8de9d6a857c2
aziz2719 <davletovaziz511@gmail.com>
2021-10-19 23:20:55 +0600

    update

--- course/settings.py
+++ course/settings.py
@@ -106,21 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #django_heroku.settings(locals())

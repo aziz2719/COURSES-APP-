f739fca33f7c203089c6b6d85dab115e9870f668
aziz2719 <davletovaziz511@gmail.com>
2021-10-19 23:06:30 +0600

    update

--- course/settings.py
+++ course/settings.py
@@ -108,6 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #django_heroku.settings(locals())
 MEDIA_URL = '/media/'
